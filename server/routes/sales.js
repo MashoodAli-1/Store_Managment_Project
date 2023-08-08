2fcf86d2b10d80007ba3c84b7de6b38cacd2bf7f
Anas Abdullah <anasabdullah1409@gmail.com>
2023-08-08 15:23:30 +0500

    add sales backend

---
+++ server/routes/sales.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  createSalesRecord,
+  getAllSalesRecords,
+  getSalesRecordByName,
+  updateSalesRecordById,
+  deleteSalesRecordById,
+} from "../controller/sales.js";
+
+const router = express.Router();
+
+router
+  .post("/", createSalesRecord) // Create a new sales record
+  .get("/all", getAllSalesRecords) // Get all sales records
+  .get("/one", getSalesRecordByName) // Get a specific sales record by cname
+  .put("/:id", updateSalesRecordById) // Update a sales record by id
+  .delete("/:id", deleteSalesRecordById); // Delete a sales record by id
+
+export default router;
